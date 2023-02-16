710f3a8ba64c92ac2d261d0747c21a3867bda51f
Gabriel Henrique <gabrielHsantos2018@gmail.com>
2023-02-16 14:17:04 -0300

    Initial commit

---
+++ src/main/java/br/com/etec/gabriel/cursoapipw/CursoapipwApplication.java
@@ -0,0 +1,13 @@
+package br.com.etec.gabriel.cursoapipw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoapipwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoapipwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etec/gabriel/cursoapipw/CursoapipwApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etec.gabriel.cursoapipw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoapipwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
