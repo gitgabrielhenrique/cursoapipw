2a41874609120bf3d6b7ee88bf8490951ad8bdea
gabriel henrique dos santos <gabrielHsantos2018@gmail.com>
2023-03-23 08:29:37 -0300

    finalizacao do banco

--- src/main/resources/db/migration/V001__Criar_tabela_Cidade_Curso.sql
+++ src/main/resources/db/migration/V001__Criar_tabela_Cidade_Curso.sql
@@ -1,25 +1,25 @@
-Create table Cidade(
+Create table cidade(
 id int not null primary key auto_increment,
 uf VARCHAR(45),
 nomecidade VARCHAR(150)
 
 
 
-)
+);
 
-insert into Cidade(nomecidade) values ('Lencois paulista');
-insert into Cidade(nomecidade) values ('Bauru');
-insert into Cidade(nomecidade) values ('Sao Paulo');
-insert into Cidade(nomecidade) values ('Macatuba');
+insert into cidade(id,uf,nomecidade) values (1,'Distrito Federal','Macatuba');
+insert into cidade(id,uf,nomecidade) values (2,'Rio de Janeiro','Rio de Janeiro');
+insert into cidade(id,uf,nomecidade) values (3,'Sao Paulo','Lencois Paulista');
+insert into cidade(id,uf,nomecidade) values (4,'Tocantins','Palmas');
 
-Create table Curso(
+Create table curso(
 id int not null primary key auto_increment,
 nomecurso VARCHAR(150)
 
 
-)
-insert into Curso(nomecurso) values ('Desenvolvimento de Sistemas');
-insert into Curso(nomecurso) values ('adiministracao');
-insert into Curso(nomecurso) values ('quimica');
-insert into Curso(nomecurso) values ('marketing');
+);
+insert into curso(id,nomecurso) values (1,'Desenvolvimento de Sistemas');
+insert into curso(id,nomecurso) values (2,'adiministracao');
+insert into curso(id,nomecurso) values (3,'quimica');
+insert into curso(id,nomecurso) values (4,'marketing');
 
--- src/main/resources/db/migration/V002__Criar_tabela_Aluno.sql
+++ src/main/resources/db/migration/V002__Criar_tabela_Aluno.sql
@@ -1,16 +1,17 @@
-Create Table Aluno(
-id int not null primary key auto_increment,
+Create Table aluno(
+id bigint not null primary key auto_increment,
 nomealuno VARCHAR(300),
-cursoid BigIint not null,
-cidadeid BigIint not null
+cursoid int not null,
+cidadeid int not null
 
 
+);
 
-)
-Alter table Aluno add constraint FK_aluno_curso FOREIGN key (cursoid) references curso(id);
-Alter table Aluno add constraint FK_aluno_cidade FOREIGN key (cidadeid) references cidade(id);
 
-insert into Aluno(nomealuno) values('gabriel');
-insert into Aluno(nomealuno) values('monique');
-insert into Aluno(nomealuno) values('mirella');
-insert into Aluno(nomealuno) values('sarah');
+Alter table aluno add constraint FK_aluno_curso FOREIGN key (cursoid) references curso(id);
+Alter table aluno add constraint FK_aluno_cidade FOREIGN key (cidadeid) references cidade(id);
+
+insert into aluno(id,nomealuno,cursoid,cidadeid) values(1,'gabriel',1,1);
+insert into aluno(id,nomealuno,cursoid,cidadeid) values(2,'monique',2,2);
+insert into aluno(id,nomealuno,cursoid,cidadeid) values(3,'mirella',3,3);
+insert into aluno(id,nomealuno,cursoid,cidadeid) values(4,'sarah',4,4);
