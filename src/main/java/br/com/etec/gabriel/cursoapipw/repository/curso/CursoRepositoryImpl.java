7b847c392252a640eca85cb9475672d7e7c62912
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-05-18 08:53:52 -0300

    Update CursoRepositoryImpl.java

--- src/main/java/br/com/etec/gabriel/cursoapipw/repository/curso/CursoRepositoryImpl.java
+++ src/main/java/br/com/etec/gabriel/cursoapipw/repository/curso/CursoRepositoryImpl.java
@@ -2,12 +2,14 @@ package br.com.etec.gabriel.cursoapipw.repository.curso;
 
 import br.com.etec.gabriel.cursoapipw.model.Curso;
 import br.com.etec.gabriel.cursoapipw.repository.filter.CursoFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.util.Predicates;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -18,30 +20,33 @@ import java.util.List;
 public class CursoRepositoryImpl implements CursoRepositoryQuery {
 
   @PersistenceContext
-   private EntityManager manager;
+  private EntityManager manager;
 
-    @Override
-    public Page<Curso> Filtrar(CursoFilter cursoFilter, Pageable pageable) {
+  @Override
+  public Page<Curso> Filtrar(CursoFilter cursoFilter, Pageable pageable) {
 
-      CriteriaBuilder builder= manager.getCriteriaBuilder();
+    CriteriaBuilder builder = manager.getCriteriaBuilder();
 
-      CriteriaQuery<Curso> criteria =builder.createQuery(Curso.class);
-      Root<Curso> root= criteria.from(Curso.class);
+    CriteriaQuery<Curso> criteria = builder.createQuery(Curso.class);
+    Root<Curso> root = criteria.from(Curso.class);
 
-      Predicate[] predicates = criarRestricoes(cursoFilter,builder,root);
-criteria.where(predicates);
+    Predicate[] predicates = criarRestricoes(cursoFilter, builder, root);
+    criteria.where(predicates);
+criteria.orderBy(builder.asc(root.get("nomecurso")));
+    TypedQuery<Curso>  query= manager.createQuery(criteria);
 
-
-
-
-
-
-      return null;
-    }
+    return null;
+  }
 
   private Predicate[] criarRestricoes(CursoFilter cursoFilter, CriteriaBuilder builder, Root<Curso> root) {
 
-    List<Predicates> predicates = new ArrayList<>();
-    if(StringUtils)
+    List<Predicate> predicates = new ArrayList<>();
+    if (!StringUtils.isAllEmpty(cursoFilter.getNomecurso())) {
+
+      predicates.add(builder.like(builder.lower(root.get("nomecurso")),
+         "%"+ cursoFilter.getNomecurso().toLowerCase()));
     }
+return  predicates.toArray(new Predicate[predicates.size()]);
+  }
 }
+
