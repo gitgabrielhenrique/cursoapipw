661fae3240509b6465c8495e3dc3d3640a0d2a0e
gitgabrielhenrique <gabrielHsantos2018@gmail.com>
2023-06-05 09:47:11 -0300

    cidade filter

--- src/main/java/br/com/etec/gabriel/cursoapipw/repository/cidade/CidadeRepositoryQuery.java
+++ src/main/java/br/com/etec/gabriel/cursoapipw/repository/cidade/CidadeRepositoryQuery.java
@@ -1,4 +1,8 @@
 package br.com.etec.gabriel.cursoapipw.repository.cidade;
-
+import br.com.etec.gabriel.cursoapipw.model.Cidade;
+import br.com.etec.gabriel.cursoapipw.repository.filter.CidadeFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 public interface CidadeRepositoryQuery {
+  public Page<Cidade> filtrar(CidadeFilter cidadeFilter, Pageable pageable);
 }
--- src/main/java/br/com/etec/gabriel/cursoapipw/resource/CidadeResource.java
+++ src/main/java/br/com/etec/gabriel/cursoapipw/resource/CidadeResource.java
@@ -2,7 +2,10 @@ package br.com.etec.gabriel.cursoapipw.resource;
 
 import br.com.etec.gabriel.cursoapipw.model.Cidade;
 import br.com.etec.gabriel.cursoapipw.repository.CidadeRepository;
+import br.com.etec.gabriel.cursoapipw.repository.filter.CidadeFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,12 @@ import java.util.List;
 public class CidadeResource {
     @Autowired
     private CidadeRepository cidadeRepository;
+  @GetMapping()
+  private Page<Cidade> query(CidadeFilter cidadeFilter, Pageable pageable)
+  {
+    return cidadeRepository.filtrar(cidadeFilter, pageable);
+  }
+
 
     @GetMapping("/todos")
     public List<Cidade> ListarTodasCidades(){
